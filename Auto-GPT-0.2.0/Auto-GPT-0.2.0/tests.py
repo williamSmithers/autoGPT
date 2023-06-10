7fe02ab1a87e4e769ce16ec0c4623f9baea6e54c
David Smith <68123366+dsmithers139@users.noreply.github.com>
2023-06-10 15:24:14 -0400

    Delete Auto-GPT-0.2.0/Auto-GPT-0.2.0 directory

--- Auto-GPT-0.2.0/Auto-GPT-0.2.0/tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-if __name__ == "__main__":
-    # Load all tests from the 'autogpt/tests' package
-    suite = unittest.defaultTestLoader.discover("autogpt/tests")
-
-    # Run the tests
-    unittest.TextTestRunner().run(suite)
